b26f31d0cf383b218151ed5de54dda9aa79e04a3
mamsdeveloper <butvin.mihail@yandex.ru>
2023-04-09 22:21:27 +0300

    add develop router and meta info page

--- BotMicro/web/routers/__init__.py
+++ BotMicro/web/routers/__init__.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+
+from .develop import develop_router
 from .webhook import webhook_router
 
 __all__ = ['root_router']
@@ -6,3 +8,4 @@ __all__ = ['root_router']
 
 root_router = APIRouter()
 root_router.include_router(webhook_router)
+root_router.include_router(develop_router)
