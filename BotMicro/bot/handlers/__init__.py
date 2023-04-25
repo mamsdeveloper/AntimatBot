a6533f64d7a7a394d8f46ff2c3199ab6bc3c594a
mamsdeveloper <butvin.mihail@yandex.ru>
2023-04-25 03:25:15 +0300

    add error handler

--- BotMicro/bot/handlers/__init__.py
+++ BotMicro/bot/handlers/__init__.py
@@ -1,8 +1,10 @@
 from aiogram import Router
 
+from .error import router as error_router
 from .groups import router as groups_router
-from .private import  router as private_router
+from .private import router as private_router
 
 root_router = Router()
 root_router.include_router(private_router)
 root_router.include_router(groups_router)
+root_router.include_router(error_router)
