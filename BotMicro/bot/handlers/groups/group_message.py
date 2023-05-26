951e3518ea24ca0d46825a08f8b42fa3b7103de0
butvinm <butvin.mihail@yandex.ru>
2023-05-26 20:12:21 +0300

    Adjust event changes
    - Use new events methods
    - Update profanity event text

--- BotMicro/bot/handlers/groups/group_message.py
+++ BotMicro/bot/handlers/groups/group_message.py
@@ -1,6 +1,4 @@
-from multiprocessing import process
 from os import getenv
-from time import time
 
 from aiogram import Bot, F, Router
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
@@ -9,14 +7,14 @@ from odetam.exceptions import ItemNotFound
 from analysis.checking import (check_full_words, check_partial_words,
                                check_profanity)
 from analysis.normilize import get_normalized_text
-from bot import messages
-from bot.callbacks.event_message import (BanMemberCallback,
-                                         DeleteMessageCallback)
-from bot.utils.message_delete_event import message_delete_event
-from bot.utils.spread import SendMessage, spread_messages
+
+from bot.messages import PROFANITY_EVENT
+from bot.utils.events import message_delete_event, profanity_filter_event
+from bot.utils.spread import SendMessage, forward_messages, spread_messages
 from models import Dictionary, Group
 from models.chat import Chat
 from models.member import Member
+from utils.logging import log
 
 router = Router()
 
@@ -27,56 +25,57 @@ async def group_message_handler(message: Message, bot: Bot, group: Group) -> Non
     if not message.text:
         return
 
-    try:
-        member: Member = await Member.get(str(message.from_user.id))
-    except ItemNotFound:
-        member = Member(key=str(message.from_user.id),
-                        strikes_count={group.key: 0})
+    member = await Member.get_or_none(str(message.from_user.id))
+    if not member:
+        member = Member(
+            key=str(message.from_user.id),
+            strikes_count={group.key: 0}
+        )
 
     member.messages_count.setdefault(group.key, 0)
     member.messages_count[group.key] += 1
-    await member.save()
+    await member.save()  # type: ignore
 
-    if member.messages_count.get(group.key, 0) >= int(getenv('MESSAGES_THRESHOLD', 5)):
+    messages_threshold = int(getenv('MESSAGES_THRESHOLD', 5))
+    if member.messages_count.get(group.key, 0) >= messages_threshold:
         return
 
-    text_without_excludes = message.text
-    text_without_excludes = text_without_excludes.replace(
+    text = message.text
+    text = text.replace(
         'https://t.me/Yasnosvet_talks', '')
-    text_without_excludes = text_without_excludes.replace(
+    text = text.replace(
         'https://t.me/Yasnosvet_thanks', '')
-    text_without_excludes = text_without_excludes.replace(
+    text = text.replace(
         'https://t.me/Yasnosvet_ask', '')
-    text_without_excludes = text_without_excludes.replace(
+    text = text.replace(
         'https://taplink.cc/pavelangel369', '')
 
-    try:
-        dictionary: Dictionary = await Dictionary.get(group.key)
-    except ItemNotFound:
-        return
+    dictionary: Dictionary = await Dictionary.get(group.key)
 
-    full_check_result = check_full_words(
-        text_without_excludes, dictionary.full_words)
+    full_check_result = check_full_words(text, dictionary.full_words)
     if full_check_result:
-        await message_delete_event(member, message, f'слово "{full_check_result}"')
+        await message_delete_event(
+            group, member, message, f'слово "{full_check_result}"', bot)
         return
 
-    partial_search_result = check_partial_words(
-        text_without_excludes, dictionary.partial_words)
+    partial_search_result = check_partial_words(text, dictionary.partial_words)
     if partial_search_result:
         word, part = partial_search_result
         if part:
-            await message_delete_event(member, message, f'часть "{part}" в слове "{word}"')
+            await message_delete_event(
+                group, member, message, f'часть "{part}" в слове "{word}"', bot)
         else:
-            await message_delete_event(member, message, f'слово "{part}"')
+            await message_delete_event(
+                group, member, message, f'слово "{part}"', bot)
         return
 
-    normalized_text = get_normalized_text(text_without_excludes)
+    normalized_text = get_normalized_text(text)
 
     full_check_result = check_full_words(
         normalized_text, dictionary.full_words)
     if full_check_result:
-        await message_delete_event(member, message, f'слово "{full_check_result}"')
+        await message_delete_event(
+            group, member, message, f'слово "{full_check_result}"', bot)
         return
 
     partial_search_result = check_partial_words(
@@ -84,44 +83,16 @@ async def group_message_handler(message: Message, bot: Bot, group: Group) -> Non
     if partial_search_result:
         word, part = partial_search_result
         if part:
-            await message_delete_event(member, message, f'часть "{part}" в слове "{word}"')
+            reason = f'часть "{part}" в слове "{word}"'
         else:
-            await message_delete_event(member, message, f'слово "{part}"')
+            reason = f'слово "{part}"'
+
+        await message_delete_event(
+            group, member, message, reason, bot)
+
         return
 
     profanity_check_result = check_profanity(message.text)
     if profanity_check_result:
-        profanity_event_text = messages.TEST_FILTER.format(
-            text=message.text,
-            word=profanity_check_result
-        )
-        profanity_event_markup = InlineKeyboardMarkup(inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text='Удалить сообщение',
-                    callback_data=DeleteMessageCallback(
-                        chat_id=message.chat.id,
-                        message_id=message.message_id
-                    ).pack()
-                ),
-                InlineKeyboardButton(
-                    text='Забанить',
-                    callback_data=BanMemberCallback(
-                        chat_id=message.chat.id,
-                        user_id=message.from_user.id
-                    ).pack()
-                )
-            ]
-        ])
-        profanity_event_message = SendMessage(
-            text=profanity_event_text,
-            reply_markup=profanity_event_markup
-        )
-
-        admins_chats: list[Chat] = await Chat.query(Chat.groups.contains(group.key))
-        admins_chats_ids = [int(chat.key) for chat in admins_chats]
-        await spread_messages(
-            admins_chats_ids,
-            [profanity_event_message],
-            bot
-        )
+        await profanity_filter_event(
+            group, member, message, profanity_check_result, bot)
